f16b9d986aee5d22c4051dca2f303abe235f2eb6
youssef ameachaq <youssame@zoho.com>
2021-10-19 16:18:23 +0100

    clean

--- app/pages/dashboard/saved.js
+++ app/pages/dashboard/saved.js
@@ -1,16 +1,7 @@
 import DashboardLayout from "../../layouts/dashboard-layout";
 import Saved from "../../modules/Feeds/Saved";
-import {useRouter} from "next/router";
-import {useAuth0} from "@auth0/auth0-react";
-import {Instagram} from "react-content-loader";
 
 export default () => {
-    const router = useRouter();
-    const { isAuthenticated, isLoading } = useAuth0();
-    if (isLoading)
-        return <Instagram />;
-    if (!isAuthenticated)
-        router.push("/auth");
     return (
         <DashboardLayout>
             <Saved />
